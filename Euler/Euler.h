e6f2241b8357dfdd9dec49a8d1be719ecb08e374
Dan Esrey <danesrey@dans-air.lan>
2016-09-21 13:36:08 -0400

    Initial Commit

---
+++ Euler/Euler.h
@@ -0,0 +1,19 @@
+//
+//  Euler.h
+//  Euler
+//
+//  Created by Dan Esrey on 2016/21/09.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Euler.
+FOUNDATION_EXPORT double EulerVersionNumber;
+
+//! Project version string for Euler.
+FOUNDATION_EXPORT const unsigned char EulerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Euler/PublicHeader.h>
+
+
